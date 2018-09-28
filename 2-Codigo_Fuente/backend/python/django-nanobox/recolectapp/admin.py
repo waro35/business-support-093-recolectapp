2c3bf2c603e15ce9e98f61f9d42bfff417ff159f
John Fonseca <johnf.biker@gmail.com>
2018-09-28 11:25:43 -0500

    Creacion app python con nanobox

---
+++ 2-Codigo_Fuente/backend/python/django-nanobox/recolectapp/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Ubicacion, Localidad, Usuario
+
+# Register your models here.
+admin.site.register(Ubicacion)
+admin.site.register(Localidad)
+admin.site.register(Usuario)
